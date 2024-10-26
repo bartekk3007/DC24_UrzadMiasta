ede023df1835134391753c74d7ad99a61b2cb6ba
Dominik Noga <dominik.noga@felten-group.com>
2024-10-26 19:23:45 +0200

    added Angular UI

---
+++ city-hall-frontend/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { FormComponent } from './components/features/form/form.component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: FormComponent
+    }
+];
