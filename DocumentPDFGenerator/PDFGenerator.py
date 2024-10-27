b5a08c6b5fdafc1f76ef2ad276acf325128d78d0
Rainflow1 <kuba.kriegel@gmail.com>
2024-10-27 12:30:29 +0100

    Simple generation

--- DocumentPDFGenerator/PDFGenerator.py
+++ DocumentPDFGenerator/PDFGenerator.py
@@ -1,16 +1,72 @@
 import pymupdf
-import json
-from jsonschema import validate
+import json as JSON
+import jsonschema
+import io
+
+
+class PDFGenerator:
+
+    def __init__(self):
+        pass
+
+    def __isValid(self, json):
+
+        schema = None
+
+        with open("./DocumentPDFGenerator/WniosekJsonSchema.json", encoding="utf-8") as f:
+            schema = JSON.load(f)
+
+        try:
+            jsonschema.validate(json, schema)
+        except jsonschema.exceptions.ValidationError as e:
+            print(f"Validation error: {e}")
+            return False
+        except jsonschema.exceptions.SchemaError as e:
+            print(f"Schema error: {e}")
+            return False
+        finally:
+            return True
+
+    def render(self, json):
+
+        if(not self.__isValid(json)):
+            return
+
+        pdf = io.BytesIO()
+        writer = pymupdf.DocumentWriter(pdf)
+        story = pymupdf.Story()
+        body = story.body
+
+        for n, (k, v) in enumerate(json.items(), 1):
+            
+            paragraph = body.add_paragraph()
+            paragraph.add_text(f"{n}: {k}")
+            body.add_horizontal_line()
+
+            pass
+
+        mediaBox = pymupdf.paper_rect("A4")
+        where = mediaBox + (36, 36, -36, -36)
+        more = 1
+        while more:
+            device = writer.begin_page(mediaBox)  # make new page
+            more, _ = story.place(where)
+            story.draw(device)
+            writer.end_page()
+
+        writer.close()
+
+        return pdf
+
+    pass
 
 
 if __name__ == "__main__":
 
-    schema = None
+    generator = PDFGenerator()
 
-    with open("./DocumentPDFGenerator/WniosekJsonSchema.json", encoding="utf-8") as f:
-        schema = json.load(f)
-
-    testJson = {
+    testJson = """
+    {
         "daneOsobowe":{
             "imie": "Janusz Marcin",
             "nazwisko": "Kowalski",
@@ -34,5 +90,9 @@ if __name__ == "__main__":
             "podpisWnioskodawcy": "Tak"
         }
     }
+    """
 
-    print(validate(testJson, schema))
+    pdf = generator.render(JSON.loads(testJson))
+
+    with open("test.pdf", "wb") as f:
+        f.write(pdf.getbuffer())
