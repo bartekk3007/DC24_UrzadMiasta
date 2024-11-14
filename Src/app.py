2b26f28a2689a8cb6b9416c09750b403d7d52985
Rainflow1 <kuba.kriegel@gmail.com>
2024-11-14 13:56:21 +0100

    Deleted temp route

--- Src/app.py
+++ Src/app.py
@@ -5,15 +5,6 @@ import json as JSON
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def form_page():
-    if request.method == 'POST':
-        user_name = request.form['name']
-        user_surname = request.form['surname']
-        user_age = request.form['age']
-        return f'Witam {user_name} {user_surname}, masz {user_age} lat!'
-    return render_template('form.html')
-
 @app.route('/generatePdf', methods=['POST'])
 def generate_pdf():
     if request.method == 'POST':
